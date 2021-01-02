de1e6ad56f3eae0ab199ef4ae830f38a3f4fed4b
shashank <49537882+shashank-rv@users.noreply.github.com>
2021-01-02 16:20:02 +1000

    Add files via upload

---
+++ flaskblog.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html')
+
+
+
+def var_name_return(var_name):
+    name_list = ["self."+ i +" " +  "=" + " " + i for i in var_name]
+    return name_list 
+
+
+
+@app.route('/', methods=['GET','POST'])
+
+def constructor():
+    text = request.form['text']
+    class_name = request.form['class']
+    parsed_text = text.split(",")
+    parsed_list = var_name_return(parsed_text)
+    list2 = ["class",class_name,"():","def __init__","(self,","):"]
+    total_list  = list2+parsed_list
+    return render_template('home.html', var1 = total_list,var2 = parsed_text,var3 = class_name)
+
+
+
+# @app.route("/about")
+
+# def about():
+#     return render_template('about.html')
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
+
+
+
